3dc4a958dbe517f765d62b9210311a52b0ab169a
raquelmgalbuquerque <81765141+raquelmgalbuquerque@users.noreply.github.com>
2023-01-08 16:23:09 +0000

    Clean code

--- apod.js
+++ apod.js
@@ -1,8 +1,6 @@
 function displayApod(date) {
   let apod = {};
 
-  // console.log(date);
-
   let url =
     date === "today"
       ? "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"
@@ -12,7 +10,6 @@ function displayApod(date) {
     .then((res) => res.json())
     .then((json) => {
       apod = json;
-      // console.log(apod);
 
       apodCopyright =
         apod.copyright === undefined
@@ -37,8 +34,9 @@ function todayDate() {
 window.onload = () => {
   // I want to fetch today's picture when I load the page...
   displayApod("today");
-  // ... and block the input maximum to today's date.
+  // ... block the input maximum to today's date...
   todayDate();
+  // ... and show some random APODs at the bottom of the page.
   displayRandomApods();
 };
 
@@ -57,7 +55,6 @@ function displayRandomApods() {
     .then((res) => res.json())
     .then((json) => {
       apods = json;
-      // console.log(apods);
 
       let randomApodContainer = document.querySelector(
         ".random-apod-container"
@@ -70,81 +67,3 @@ function displayRandomApods() {
       });
     });
 }
-
-// =====================================================================
-// This is the backup plan when I do too many API requests
-// =====================================================================
-// const apiData = [
-//   {
-//     copyright: "Panther Observatory",
-//     date: "2008-07-19",
-//     explanation:
-//       "Young star cluster M16 is surrounded by natal clouds of cosmic dust and glowing gas also known as The Eagle Nebula. This beautifully detailed image of the region includes fantastic shapes made famous in well-known Hubble Space Telescope close-ups of the starforming complex. Described as elephant trunks or Pillars of Creation, dense, dusty columns rising near the center are light-years in length but are gravitationally contracting to form stars. Energetic radiation from the cluster stars erodes material near the tips, eventually exposing the embedded new stars. Extending from the upper left edge of the nebula is another dusty starforming column known as the Fairy of Eagle Nebula. M16 and the Eagle Nebula lie about 7,000 light-years away, an easy target for binoculars or small telescopes in a nebula rich part of the sky toward the split constellation Serpens Cauda (the tail of the snake).   digg_url = 'http://apod.nasa.gov/apod/ap080719.html'; digg_skin = 'compact';",
-//     hdurl: "https://apod.nasa.gov/apod/image/0807/M16F970_schedler.jpg",
-//     media_type: "image",
-//     service_version: "v1",
-//     title: "M16 and the Eagle Nebula",
-//     url: "https://apod.nasa.gov/apod/image/0807/M16F970_schedler800.jpg",
-//   },
-//   {
-//     date: "2002-04-27",
-//     explanation:
-//       "Aloha! With the graceful arc of Earth's limb in the background, the Hawaiian Island archipelago is visible in this stunning photo taken by the astronauts onboard the shuttle Discovery in October of 1988. Along with popular beaches and tropical resorts, these volcanic islands offer extreme elevations with dark, dry, cloudless skies. Consequently they have also become popular sites for large and sophisticated ground based telescopes. The peak of Mauna Kea, on the Big Island (upper left), boasts an impressive array of astronomical instruments including twin Kecks, the Canada-France-Hawaii Telescope, the NASA IRTF, the JCMT and UKIRT, the Subaru and the Gemini Telescope Project. The dormant volcanic cone of Haleakala on Maui (just below the Big Island) is home to the Air Force Maui Optical Station and the Mees Solar Observatory. Mahalo nui loa!",
-//     hdurl: "https://apod.nasa.gov/apod/image/0204/hawaii_sts26_big.jpg",
-//     media_type: "image",
-//     service_version: "v1",
-//     title: "Hawaii",
-//     url: "https://apod.nasa.gov/apod/image/0204/hawaii_sts26.jpg",
-//   },
-//   {
-//     date: "2014-05-10",
-//     explanation:
-//       "The Flame Nebula stands out in this optical image of the dusty, crowded star forming regions toward Orion's belt, a mere 1,400 light-years away. X-ray data from the Chandra Observatory and infrared images from the Spitzer Space Telescope can take you inside the glowing gas and obscuring dust clouds though. Swiping your cursor (or clicking the image) will reveal many stars of the recently formed, embedded cluster NGC 2024, ranging in age from 200,000 years to 1.5 million years young. The X-ray/infrared composite image overlay spans about 15 light-years across the Flame's center. The X-ray/infrared data also indicate that the youngest stars are concentrated near the middle of the Flame Nebula cluster. That's the opposite of the simplest models of star formation for the stellar nursery that predict star formation begins in the denser center of a molecular cloud core. The result requires a more complex model; perhaps star formation continues longer in the center, or older stars are ejected from the center due to subcluster mergers.",
-//     hdurl: "https://apod.nasa.gov/apod/image/1405/Flamessc2014-04a_Med.jpg",
-//     media_type: "image",
-//     service_version: "v1",
-//     title: "Inside the Flame Nebula",
-//     url: "https://apod.nasa.gov/apod/image/1405/flame_optical.jpg",
-//   },
-//   {
-//     copyright: "G\ufffdran Strand",
-//     date: "2018-11-19",
-//     explanation:
-//       "This is a gibbous Moon. More Earthlings are familiar with a full moon, when the entire face of Luna is lit by the Sun, and a crescent moon, when only a sliver of the Moon's face is lit.  When more than half of the Moon is illuminated, though, but still short of full illumination, the phase is called gibbous.  Rarely seen in television and movies, gibbous moons are quite common in the actual night sky. The featured image was taken in J\ufffdmtland, Sweden near the end of last month.  That gibbous moon turned, in a few days, into a crescent moon, and then a new moon, then back to a crescent, and a few days ago back to gibbous.  And this same gibbous moon is visible again tonight, leading up to the Full Beaver Moon that occurs Friday night.  Setting up to capture a picturesque gibbous moonscape, the photographer was quite surprised to find an airplane, surely well in the foreground, appearing to fly past it.",
-//     hdurl: "https://apod.nasa.gov/apod/image/1811/GibbousMoon_Strand_1500.jpg",
-//     media_type: "image",
-//     service_version: "v1",
-//     title: "Gibbous Moon beyond Swedish Mountain",
-//     url: "https://apod.nasa.gov/apod/image/1811/GibbousMoon_Strand_960.jpg",
-//   },
-//   {
-//     copyright: "Stephane Vetter",
-//     date: "2018-11-23",
-//     explanation:
-//       "On November 17, just an hour before sunrise, this bright and colorful meteor flashed through clear predawn skies. Above a sea of clouds this striking autumn morning's moment was captured from Hochblauen, a prominent 1165 meter high summit in southern Germany's Black Forest. Shining through the twilight, Sirius as well as the familiar stars of Orion are recognizable near the southwestern horizon, and the meteor seems headed right for the hunter's belt and sword. Still, as part of the annual Leonid meteor shower, the meteor trail does point back to the shower's radiant. The constellation Leo is high above the horizon and off the top left of the frame.",
-//     hdurl: "https://apod.nasa.gov/apod/image/1811/leonidSV_1300.jpg",
-//     media_type: "image",
-//     service_version: "v1",
-//     title: "Good Morning Leonid",
-//     url: "https://apod.nasa.gov/apod/image/1811/leonidSV_1000.jpg",
-//   },
-// ];
-
-// function displayRandomApods() {
-//   // Lets sort the random pictures by date
-//   let apods = apiData;
-//   //   .map((obj) => {
-//   //     return { ...obj, date: new Date(obj.date) };
-//   //   })
-//   //   .sort((objA, objB) => Number(objA.date) - Number(objB.date));
-//   console.log(apods);
-
-//   let randomApodContainer = document.querySelector(".random-apod-container");
-
-//   apods.forEach((apod) => {
-//     console.log(apod.date);
-//     let randomApodElement = document.createElement("div");
-//     randomApodElement.innerHTML = `<img class="random-apod-image" src="${apod.url}" alt="${apod.title}" onclick="displayApod('${apod.date}')"/>`;
-//     randomApodContainer.appendChild(randomApodElement);
-//   });
-// }
